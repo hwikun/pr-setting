584850bde7adc6ae104bfb2846ddd1cd7b891d74
Hwikun <leah8608@gmail.com>
2022-12-28 10:45:40 +0900

    Initial Commit

---
+++ pr-setting/pr_settingApp.swift
@@ -0,0 +1,17 @@
+//
+//  pr_settingApp.swift
+//  pr-setting
+//
+//  Created by hwikun on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct pr_settingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
